c187101df398f5358a9a703607a9bb792e4a5e1c
IvanVF <fedinivanvladimirovich@mail.ru>
2021-05-08 17:22:00 +0700

    1st commit

---
+++ src/main/java/com/fprojects/RESTquestionnaire/ResTquestionnaireApplication.java
@@ -0,0 +1,13 @@
+package com.fprojects.RESTquestionnaire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResTquestionnaireApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResTquestionnaireApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fprojects/RESTquestionnaire/ResTquestionnaireApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fprojects.RESTquestionnaire;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResTquestionnaireApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
